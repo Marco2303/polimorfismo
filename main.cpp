af7e9d55ef9c88b2ddd04503bcb98aca89bc704e
Marco2303 <marcoto@tiscali.it>
2021-06-25 11:51:50 +0200

    Passaggio parametri a funzione by reference

--- main.cpp
+++ main.cpp
@@ -6,12 +6,27 @@
 #include "f500.h"
 #include "panda.h"
 
+
+void funcgenerica(automobile *a){
+    std::cout << "Richiamo la valutazione da funziona ---> " << a->valutazione() << "\n";
+    delete a;
+}
+
+void funcgenericaref(automobile &a){
+    std::cout << "Richiamo la valutazione da funziona ---> " << a.valutazione() << "\n";
+}
+
 int main()
 {
 
+    //passaggio by reference
+    peugeot *a1 = new peugeot();
+    automobile &newauto = *a1;
+    funcgenericaref(newauto);
+
+
     //automobile *autom = new automobile();
     automobile *autom;
-
 
     autom = new fiat();
     std::cout << "da maint richiamo il metodo override di ---> " << autom->valutazione() << std::endl;
@@ -32,6 +47,8 @@ int main()
     autom = new panda();
     std::cout << "da maint richiamo il metodo override di ---> " << autom->valutazione() << std::endl;
     delete autom;
+
+    funcgenerica(new F500());
 
     std::cout << "Fine programma\n";
 
